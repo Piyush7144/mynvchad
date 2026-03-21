39e39225ebbf2660ac01f741fc604dfab5a17388
piyush <piyushyadavpiyad@gmail.com>
2026-03-21 17:42:21 +0530

    add custom plugins, transparency

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    -- event = {"BufWritePre","BufNewFile" }, -- uncomment for format on save
     opts = require "configs.conform",
   },
 
@@ -13,16 +13,67 @@ return {
     end,
   },
 
+  { "ibhagwan/fzf-lua",
+    lazy = false,
+    config = function()
+      require("fzf-lua").setup({})
+    end,
+  -- optional for icon support
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- or if using mini.icons/mini.nvim
+  -- dependencies = { "nvim-mini/mini.icons" },
+  ---@module "fzf-lua"
+  ---@type fzf-lua.Config|{}
+  ---@diagnostic disable: missing-fields
+  ---@diagnostic enable: missing-fields
+  },
+  {
+      "rmagatti/auto-session",
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+    -- log_level = 'debug',
+  }
+  },
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        file_ignore_patterns = { "node_modules", "dist", "build", ".git" },
+        layout_strategy = "horizontal",
+        layout_config = {
+          width = 0.9,
+          preview_cutoff = 120,
+        },
+      },
+    },
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "python", "markdown", "markdown_inline"
+  		},
+  	},
+  },
+
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    ft = {'markdown'},
+    opts = {},
+   },
 }
