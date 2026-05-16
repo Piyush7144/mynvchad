40a7369224537f097c1ffc686afb39fe90825d70
piyush <piyushyadavpiyad@gmail.com>
2026-05-16 18:59:20 +0530

    change init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,18 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
--- vim.opt.shell = "pwsh.exe"
 vim.opt.swapfile = false
+vim.opt.wildignore:append {
+  "node_modules/*", "dist/*", "build/*", ".git/*", ".venv/*", "*.o", "*.obj"
+}
+-- Used for Diagnostics popup
+vim.opt.updatetime = 250
+
+-- =========================
+-- Windows shell
+-- =========================
+if vim.fn.has("win32") == 1 then
+  vim.o.shell         = "pwsh.exe"
+end
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
@@ -37,3 +48,19 @@ require "autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+-- =========================
+-- Diagnostics (single merged config call)
+-- =========================
+vim.diagnostic.config({
+  virtual_text = true,
+  float = { border = "rounded", focusable = false, header = "", prefix = "" },
+})
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focus = false, border = "rounded", scope = "cursor" })
+  end,
+})
+
+
